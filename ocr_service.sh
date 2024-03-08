e8fbedc3ae1284a3677d048b69ae75d64dfaebac
zhipopoo <37234819+zhipopoo@users.noreply.github.com>
2024-03-08 17:44:11 +0800

    feat: add v1.4

--- ocr_service.sh
+++ ocr_service.sh
@@ -1,15 +1,4 @@
 #!/bin/bash
 
-service mysql restart
-
-mysql <<EOF
-DROP DATABASE IF EXISTS ocr;
-CREATE DATABASE ocr;
-use ocr;
-source ocr.sql;
-
-exit
-EOF
-
 ulimit -n 2000
 xvfb-run java -jar app.jar -Xms1024m -Xmx2048m
